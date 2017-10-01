91377071b9df8831f6430055fd85d30eadbf15bb
Diego Andrés <andruco3@gmail.com>
2017-10-01 18:51:57 -0500

    Iteracion 1, Retonar Arreglo con el numero de elementos - con N Numeros (Rojo)

--- estadisticasTest.py
+++ estadisticasTest.py
@@ -14,3 +14,8 @@ class estadisticasTest(TestCase):
     def test_getEstadisticas_DosNumeros(self):
         estadistica=Estadisticas().getEstadisticas("1,4")
         self.assertEquals(estadistica[0],2,"Con dos numeros")
+
+    def test_getEstadisticas_NNumeros(self):
+        estadistica=Estadisticas().getEstadisticas("1,4,8,5")
+        self.assertEquals(estadistica[0],4,"Con N numeros")
+
