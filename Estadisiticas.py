9d4de560811196f0d9f45c268356429d2badcd8f
Diego Andrés <andruco3@gmail.com>
2017-10-01 18:50:50 -0500

    Iteracion 1, Se incluye Logica para dos Numeros de entrada (Verde)

--- Estadisiticas.py
+++ Estadisiticas.py
@@ -3,5 +3,7 @@ class Estadisticas:
     def getEstadisticas(self,cadena):
         if cadena == "":
             return [0]
+        elif "," in cadena:
+            return [2]
         else:
             return [1]
