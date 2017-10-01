3f1a87d0599b4850dc57bcf17ddf391bf7fcaa02
Diego Andrés <andruco3@gmail.com>
2017-10-01 18:40:57 -0500

    Iteracion 1, Retonar Arreglo con el numero de elementos - con un Numero (Rojo)

--- estadisticasTest.py
+++ estadisticasTest.py
@@ -3,6 +3,10 @@ from unittest import TestCase
 from Estadisiticas import Estadisticas
 
 class estadisticasTest(TestCase):
-    def test_getEstadisticas(self):
+    def test_getEstadisticas_vacia(self):
         estadistica=Estadisticas().getEstadisticas("")
         self.assertEquals(estadistica[0],0,"Cadena vacia")
+
+    def test_getEstadisticas_UnNumero(self):
+        estadistica=Estadisticas().getEstadisticas("1")
+        self.assertEquals(estadistica[0],1,"Con un numero")
