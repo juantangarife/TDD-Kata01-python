0e31daae4a71ec5589be1207b544d018a4af2402
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-01 19:42:21 -0500

    Iteración 3 - Se agrega el test para verificar el máximo en un arreglo con un solo elemento (Rojo)

--- estadisticasTest.py
+++ estadisticasTest.py
@@ -6,22 +6,23 @@ class estadisticasTest(TestCase):
     def test_getEstadisticas_vacia(self):
         estadistica = Estadisticas().getEstadisticas("")
         self.assertEqual(estadistica[0], 0, "Cadena vacia - Num. elementos")
-        self.assertEqual(estadistica[1], None, "Cadena vacia - Mínimo")
-        self.assertEqual(estadistica[2], None, "Cadena vacia - Máximo")
+        self.assertEqual(estadistica[1], None, "Cadena vacia - Minimo")
+        self.assertEqual(estadistica[2], None, "Cadena vacia - Maximo")
 
     def test_getEstadisticas_UnNumero(self):
         estadistica = Estadisticas().getEstadisticas("5")
         self.assertEqual(estadistica[0], 1, "Con un numero - Num. elementos")
         self.assertEqual(estadistica[1], 5, "Con un numero - Minimo")
+        self.assertEqual(estadistica[2], 5, "Con un numero - Maximo")
 
     def test_getEstadisticas_DosNumeros(self):
         estadistica = Estadisticas().getEstadisticas("4,1")
-        self.assertEqual(estadistica[0],2,"Con dos numeros - Num. elementos")
-        self.assertEqual(estadistica[1],1,"Con dos numeros - Minimo")
+        self.assertEqual(estadistica[0], 2, "Con dos numeros - Num. elementos")
+        self.assertEqual(estadistica[1], 1, "Con dos numeros - Minimo")
 
 
     def test_getEstadisticas_NNumeros(self):
         estadistica = Estadisticas().getEstadisticas("5,4,8,5,2")
-        self.assertEqual(estadistica[0],5,"Con N numeros - Num. elementos")
-        self.assertEqual(estadistica[1],2,"Con N numeros - Minimo")
+        self.assertEqual(estadistica[0], 5, "Con N numeros - Num. elementos")
+        self.assertEqual(estadistica[1], 2, "Con N numeros - Minimo")
 
