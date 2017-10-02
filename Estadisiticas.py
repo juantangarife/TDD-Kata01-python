44154533e3b735370e0d1f7be58451f2e4b7227c
Diego Andrés <andruco3@gmail.com>
2017-10-01 19:22:30 -0500

    Iteracion 2, Se incluye Logica para dos números (Verde)

--- Estadisiticas.py
+++ Estadisiticas.py
@@ -3,6 +3,14 @@ class Estadisticas:
     def getEstadisticas(self,cadena):
         if cadena == "":
             return [0,None]
-        else:
+        elif "," in cadena:
             numeros = cadena.split(",")
-            return [len(numeros),int(numeros[0])]
+            if int(numeros[0]) < int(numeros[1]):
+                minimo = int(numeros[0])
+            else:
+                minimo = int(numeros[1])
+            return [len(numeros), minimo]
+        else:
+            return [1,int(cadena)]
+
+
