058d4844f268dfa07cfde9067e61f0508266c769
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-01 19:34:46 -0500

    Iteración 3 - Se añade el test para verificar el máximo en la cadena vacía (Rojo)

--- estadisticasTest.py
+++ estadisticasTest.py
@@ -1,20 +1,21 @@
 from unittest import TestCase
-
 from Estadisiticas import Estadisticas
+
 
 class estadisticasTest(TestCase):
     def test_getEstadisticas_vacia(self):
-        estadistica=Estadisticas().getEstadisticas("")
-        self.assertEquals(estadistica[0],0,"Cadena vacia - Num. elementos")
-        self.assertEquals(estadistica[1],None, "Cadena vacia")
+        estadistica = Estadisticas().getEstadisticas("")
+        self.assertEquals(estadistica[0], 0, "Cadena vacia - Num. elementos")
+        self.assertEquals(estadistica[1], None, "Cadena vacia - Mínimo")
+        self.assertEquals(estadistica[2], None, "Cadena vacia - Máximo")
 
     def test_getEstadisticas_UnNumero(self):
-        estadistica=Estadisticas().getEstadisticas("5")
-        self.assertEquals(estadistica[0],1,"Con un numero - Num. elementos")
-        self.assertEquals(estadistica[1],5,"Con un numero - Minimo")
+        estadistica = Estadisticas().getEstadisticas("5")
+        self.assertEquals(estadistica[0], 1, "Con un numero - Num. elementos")
+        self.assertEquals(estadistica[1], 5, "Con un numero - Minimo")
 
     def test_getEstadisticas_DosNumeros(self):
-        estadistica=Estadisticas().getEstadisticas("4,1")
+        estadistica = Estadisticas().getEstadisticas("4,1")
         self.assertEquals(estadistica[0],2,"Con dos numeros - Num. elementos")
         self.assertEquals(estadistica[1],1,"Con dos numeros - Minimo")
 
