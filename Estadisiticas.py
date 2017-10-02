b5cb8cd79ced086f5c8a091a661a67c615bb5d89
Diego Andrés <andruco3@gmail.com>
2017-10-01 19:27:08 -0500

    Iteracion 2, Se incluye Logica para N números (Verde)

--- Estadisiticas.py
+++ Estadisiticas.py
@@ -5,10 +5,10 @@ class Estadisticas:
             return [0,None]
         elif "," in cadena:
             numeros = cadena.split(",")
-            if int(numeros[0]) < int(numeros[1]):
-                minimo = int(numeros[0])
-            else:
-                minimo = int(numeros[1])
+            minimo = int(numeros[0])
+            for l in numeros:
+                if minimo > int(l):
+                    minimo = int(l)
             return [len(numeros), minimo]
         else:
             return [1,int(cadena)]
