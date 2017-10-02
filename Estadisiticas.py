892cd821162428a8d6e5db44a52d703d08c3f68d
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-01 20:00:08 -0500

    Iteración 4 - Se modifica el código para calcular el promedio cuando la cadena tiene 2 elementos

--- Estadisiticas.py
+++ Estadisiticas.py
@@ -4,15 +4,19 @@ class Estadisticas:
             return [0, None, None, None]
         elif "," in cadena:
             numeros = cadena.split(",")
-            minimo = int(numeros[0])
-            maximo = int(numeros[0])
+            primero = int(numeros[0])
+            num_elementos = len(numeros)
+            minimo = primero
+            maximo = primero
+            total = 0
             for l in numeros:
                 num_l = int(l)
                 if minimo > num_l:
                     minimo = num_l
                 if maximo < num_l:
                     maximo = num_l
-            return [len(numeros), minimo, maximo]
+                total += num_l
+            return [num_elementos, minimo, maximo, total/2.0]
         else:
             num = int(cadena)
             return [1, num, num, num]
